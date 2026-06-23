d0540570c1b23ce5c62baeaeabc3819fb8c9a77d
sujit-012 <sujitbanafar625@gmail.com>
2026-06-23 12:25:19 +0530

    Web tech
    learn about html and CSS Basics

---
+++ backend/Java/Task.java
@@ -0,0 +1,12 @@
+class Task {
+    public static void main(String[] args) {
+        System.out.println("Sujit Banafar");
+        System.out.println(22);
+        System.out.println(8411087916l);
+        System.out.println(5.11f);
+        System.out.println("sujit625@gmail.com");
+        System.out.println(22 / 7);
+        System.out.println(2004);
+        System.out.println(70);
+    }
+}
