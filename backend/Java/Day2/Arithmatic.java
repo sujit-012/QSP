9340f3cd3f3b1c47d264b533fca3d41ccb92f2bd
sujit-012 <sujitbanafar625@gmail.com>
2026-06-24 16:35:35 +0530

    List
    Learn about the list

---
+++ backend/Java/Day2/Arithmatic.java
@@ -0,0 +1,162 @@
+class Arithmatic {
+    public static void main(String args[]) {
+        // byte a = 100;
+        // byte b = 50;
+        // int result = a - b;
+        // System.out.println(result);
+
+        // short a = 200;
+        // short b = 500;
+        // int result = a - b;
+        // System.out.println(result);
+
+        // int a = 50000;
+        // int b = 60000;
+        // int result = a - b;
+        // System.out.println(result);
+
+        // char a = 'a';
+        // char b = 'b';
+        // int result = a - b;
+        // System.out.println(result);
+
+        // double a = 1.1;
+        // double b = 2.2;
+        // double result = a - b;
+        // System.out.println(result);
+
+        // boolean a = true;
+        // boolean b = false;
+        // boolean result = a - b;
+        // System.out.println(result);.
+
+        // String a = "sujit";
+        // String b = "Banafar";
+        // String result = a - b;
+        // System.out.println(result);
+
+        // Multiplication
+        // byte a = 100;
+        // byte b = 50;
+        // int result = a * b;
+        // System.out.println(result);
+
+        // short a = 200;
+        // short b = 500;
+        // int result = a * b;
+        // System.out.println(result);
+
+        // int a = 50000;
+        // int b = 60000;
+        // int result = a * b;
+        // System.out.println(result);
+
+        // char a = 'a';
+        // char b = 'b';
+        // int result = a * b;
+        // System.out.println(result);
+
+        // float a = 1.1f;
+        // float b = 2.2f;
+        // float result = a * b;
+        // System.out.println(result);
+
+        // double a = 1.1;
+        // double b = 2.2;
+        // double result = a * b;
+        // System.out.println(result);
+
+        // boolean a = true;
+        // boolean b = false;
+        // boolean result = a * b;
+        // System.out.println(result);
+
+        // String a = "sujit";
+        // String b = "Banafar";
+        // String result = a * b;
+        // System.out.println(result);
+
+        // Division
+        // byte a = 100;
+        // byte b = 50;
+        // int result = a / b;
+        // System.out.println(result);
+
+        // short a = 500;
+        // short b = 200;
+        // int result = a / b;
+        // System.out.println(result);
+
+        // int a = 50000;
+        // int b = 60000;
+        // int result = b / a;
+        // System.out.println(result);
+
+        // char a = 'a';
+        // char b = 'b';
+        // int result = b / a;
+        // System.out.println(result);
+
+        // float a = 1.1f;
+        // float b = 2.2f;
+        // float result = b / a ;
+        // System.out.println(result);
+
+        // double a = 1.12;
+        // double b = 2.2;
+        // double result = b / a;
+        // System.out.println(result);
+
+        // boolean a = true;
+        // boolean b = false;
+        // boolean result = a / b;
+        // System.out.println(result);
+
+        // String a = "sujit";
+        // String b = "Banafar";
+        // String result = a / b;
+        // System.out.println(result);
+
+        // Modulus
+        // byte a = 100;
+        // byte b = 50;
+        // int result = a % b;
+        // System.out.println(result);
+
+        // short a = 500;
+        // short b = 200;
+        // int result = a % b;
+        // System.out.println(result);
+
+        // int a = 50000;
+        // int b = 60000;
+        // int result = b % a;
+        // System.out.println(result);
+
+        // char a = 'a';
+        // char b = 'b';
+        // int result = b % a;
+        // System.out.println(result);
+
+        // float a = 1.1f;
+        // float b = 2.2f;
+        // float result = b % a;
+        // System.out.println(result);
+
+        // double a = 1.12;
+        // double b = 2.2;
+        // double result = b % a;
+        // System.out.println(result);
+
+        // boolean a = true;
+        // boolean b = false;
+        // boolean result = a % b;
+        // System.out.println(result);
+
+        // String a = "sujit";
+        // String b = "Banafar";
+        // String result = a % b;
+        // System.out.println(result);
+
+    }
+}
