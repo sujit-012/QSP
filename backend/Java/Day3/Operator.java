fd2fd58072194a1dd89cdfb48501d10e5f4e54a6
sujit-012 <sujitbanafar625@gmail.com>
2026-06-27 08:49:18 +0530

    CSS and JAVA
    list and table in Html and in java Operator

---
+++ backend/Java/Day3/Operator.java
@@ -0,0 +1,68 @@
+class Operator {
+
+    public static void main(String[] args) {
+        // byte a = 10;
+        // System.out.println(a);
+        // short b = a;
+        // System.out.println(b);
+        // int c = b;
+        // System.out.println(c);
+        // long d = c;
+        // System.out.println(d);
+        // float e = d;
+        // System.out.println(e);
+        // double f = e;
+        // System.out.println(f);
+        // char g = f;
+        // boolean i = f;
+
+        // short b = 10;
+        // System.out.println(b);
+        // int c = b;
+        // System.out.println(c);
+        // long d = c;
+        // System.out.println(d);
+        // float e = d;
+        // System.out.println(e);
+        // double f = e;
+        // System.out.println(f);
+        // char g = f;
+        // boolean i = f;
+
+        // int c = 10;
+        // System.out.println(c);
+        // long d = c;
+        // System.out.println(d);
+        // float e = d;
+        // System.out.println(e);
+        // double f = e;
+        // System.out.println(f);
+        // char g = f;
+        // boolean i = f;
+
+        // long d = 10;
+        // System.out.println(d);
+        // float e = d;
+        // System.out.println(e);
+        // double f = e;
+        // System.out.println(f);
+        // char g = f;
+        // boolean i = f;
+
+        // float e = 10.0f;
+        // System.out.println(e);
+        // double f = e;
+        // System.out.println(f);
+        // // char g = f;
+        // // boolean i = f;
+
+        // double f = 10.0;
+        // System.out.println(f);
+        // char g = f;
+        // boolean i = f;
+
+        // char g = 'f';
+        // System.out.println(g);
+        // boolean i = f;
+    }
+}
