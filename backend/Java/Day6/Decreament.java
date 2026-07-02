d4ef552994ec327b97c60bf7164900cc68d61172
sujit-012 <sujitbanafar625@gmail.com>
2026-07-02 08:42:53 +0530

    Increament and Decreament Operator

---
+++ backend/Java/Day6/Decreament.java
@@ -0,0 +1,16 @@
+class Decreament 
+{
+	public static void main(String[] args) 
+	{
+		int poc = 100;
+		System.out.println(--poc);
+		System.out.println(--poc);
+		System.out.println(poc--);
+		System.out.println(poc--);
+		System.out.println(--poc);
+		System.out.println(poc--);
+		System.out.println(--poc + poc--);
+		System.out.println(poc--);
+		System.out.println(poc);
+	}
+}
---
+++ backend/Java/Day6/Increament.java
@@ -0,0 +1,15 @@
+class Increament 
+{
+	public static void main(String[] args) 
+	{
+		int poc = 100;
+		System.out.println(++poc);
+		System.out.println(++poc);
+		System.out.println(poc++);
+		System.out.println(poc++);
+		System.out.println(++poc);
+		System.out.println(++poc + poc++);
+		System.out.println(poc++);
+		System.out.println(poc);
+	}
+}
